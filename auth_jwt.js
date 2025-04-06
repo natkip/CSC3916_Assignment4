c41611a77fa809c3a2328dcdf11fc93e9803ea49
natannekip <natashakippur@gmail.com>
2025-04-06 17:56:45 -0600

    updated

--- auth_jwt.js
+++ auth_jwt.js
@@ -9,6 +9,9 @@ opts.secretOrKey = process.env.SECRET_KEY;
 
 passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
     User.findById(jwt_payload.id, function (err, user) {
+        if (err) {
+            return done(err, false);
+        }
         if (user) {
             done(null, user);
         } else {
