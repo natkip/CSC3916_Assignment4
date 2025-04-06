136e44d5de719bc4a2258e0d496ca60ddb609511
natannekip <natashakippur@gmail.com>
2025-04-06 17:32:06 -0600

    reviews

--- Reviews.js
+++ Reviews.js
@@ -4,7 +4,11 @@ var Schema = mongoose.Schema;
 mongoose.connect(process.env.DB);
 
 // Movie schema
-var ReviewSchema = new Schema({
+const ReviewSchema = new Schema({
+    movieId: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie'},
+    username: String,
+    review: String,
+    rating: { type: Number, min: 0, max: 5}
 
 });
 
